7e9880ff0b4bd3a4b16ab4e5b7c2c9cec70ab5fd
AlenOmerovic2005 <alen.omerovic@htl-wels.at>
2023-02-03 09:20:43 +0100

    insert methode

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,14 +2,33 @@ package main;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Main extends Application {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)  {
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    public void interData() throws SQLException {
+        Connection con = DriverManager.getConnection("jdbc:derby://localhost:1527/D:/4AHIT/SYT/Client/JavaFX_DB/DATABASE", "username", "password");
+
+        String query = "INSERT INTO tableName (column1, column2, column3) VALUES (?, ?, ?)";
+        PreparedStatement pstmt = con.prepareStatement(query);
+
+        pstmt.setString(1, value1);
+        pstmt.setString(2, value2);
+        pstmt.setString(3, value3);
+
+        pstmt.executeUpdate();
+        con.close();
+
 
     }
 }
