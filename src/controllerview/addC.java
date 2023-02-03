0865138df2b6f13a18ac02ed7061b10e0709d599
Safa Öztürk <safa.oeztuerk@htl-wels.at>
2023-02-03 09:20:38 +0100

    JavaFX

---
+++ src/controllerview/addC.java
@@ -0,0 +1,9 @@
+package controllerview;
+
+import javafx.scene.control.TextField;
+
+public class addC {
+    public TextField tfID;
+    public TextField tfName;
+    public TextField tfWohnort;
+}
---
+++ src/model/Person.java
@@ -0,0 +1,34 @@
+package model;
+
+public class Person {
+    private int id;
+    private String name;
+
+    private String wohnort;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWohnort() {
+        return wohnort;
+    }
+
+    public void setWohnort(String wohnort) {
+        this.wohnort = wohnort;
+    }
+
+
+}
